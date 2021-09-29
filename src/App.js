2ae96dffb4286898d4cbba61377f7681c1cec860
nastygrin <nishkarsh.gautam@gmail.com>
2021-09-30 01:13:08 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,187 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import MaterialTable from 'material-table';
+import './styles/App.css';
+import axios from 'axios';
+import { Alert, AlertTitle } from '@material-ui/lab';
 
-function App() {
+
+
+const App = () => {
+
+  const [user, setUser] = useState([]);
+  const [iserror, setIserror] = useState(false);
+  const [errorMessages, setErrorMessages] = useState([]);
+
+  let columns = [
+    { title: 'NAME', field: 'name' },
+    { title: 'USERNAME', field: 'username' },
+    { title: 'EMAIL', field: 'email' },
+    { title: 'PHONE', field: 'phone' },
+    { title: 'WEBSITE', field: 'website' },
+  ]
+
+  // let data = [
+  //   { name: 'manish', username: 'traptrick', email: 'themk85@gmail.com', phone: '9999999999', website: 'https://github.com/traptrick' }
+  // ]  
+
+  useEffect(() => {
+    axios.get(`https://jsonplaceholder.typicode.com/users`)
+      .then(res => {
+        const users = res.data;
+        setUser(users);
+        // console.log(users);
+      })
+  }, [])
+
+
+
+  //function for updating the existing row details
+  const handleRowUpdate = (newData, oldData, resolve) => {
+    //validating the data inputs
+    let errorList = []
+    if (newData.name === "") {
+      errorList.push("Try Again, You didn't enter the name field")
+    }
+    if (newData.username === "") {
+      errorList.push("Try Again, You didn't enter the Username field")
+    }
+    if (newData.email === "") {
+      errorList.push("Oops!!! Please enter a valid email")
+    }
+    if (newData.phone === "") {
+      errorList.push("Try Again, Phone number field can't be blank")
+    }
+    if (newData.website === "") {
+      errorList.push("Try Again, Enter website url before submitting")
+    }
+
+    if (errorList.length < 1) {
+      axios.put(`https://jsonplaceholder.typicode.com/users/${newData.id}`, newData)
+        .then(response => {
+          const updateUser = [...user];
+          const index = oldData.tableData.id;
+          updateUser[index] = newData;
+          setUser([...updateUser]);
+          resolve()
+          setIserror(false)
+          setErrorMessages([])
+        })
+        .catch(error => {
+          setErrorMessages(["Update failed! Server error"])
+          setIserror(true)
+          resolve()
+
+        })
+    } else {
+      setErrorMessages(errorList)
+      setIserror(true)
+      resolve()
+
+    }
+  }
+
+
+  //function for deleting a row
+  const handleRowDelete = (oldData, resolve) => {
+    axios.delete(`https://jsonplaceholder.typicode.com/users/${oldData.id}`)
+      .then(response => {
+        const dataDelete = [...user];
+        const index = oldData.tableData.id;
+        dataDelete.splice(index, 1);
+        setUser([...dataDelete]);
+        resolve()
+      })
+      .catch(error => {
+        setErrorMessages(["Delete failed! Server error"])
+        setIserror(true)
+        resolve()
+      })
+  }
+
+
+  //function for adding a new row to the table
+  const handleRowAdd = (newData, resolve) => {
+    //validating the data inputs
+    let errorList = []
+    if (newData.name === "") {
+      errorList.push("Try Again, You didn't enter the name field")
+    }
+    if (newData.username === "") {
+      errorList.push("Try Again, You didn't enter the Username field")
+    }
+    if (newData.email === "") {
+      errorList.push("Oops!!! Please enter a valid email")
+    }
+    if (newData.phone === "") {
+      errorList.push("Try Again, Phone number field can't be blank")
+    }
+    if (newData.website === "") {
+      errorList.push("Try Again, Enter website url before submitting")
+    }
+
+    if (errorList.length < 1) {
+      axios.post(`https://jsonplaceholder.typicode.com/users`, newData)
+        .then(response => {
+          let newUserdata = [...user];
+          newUserdata.push(newData);
+          setUser(newUserdata);
+          resolve()
+          setErrorMessages([])
+          setIserror(false)
+        })
+        .catch(error => {
+          setErrorMessages(["Cannot add data. Server error!"])
+          setIserror(true)
+          resolve()
+        })
+    } else {
+      setErrorMessages(errorList)
+      setIserror(true)
+      resolve()
+    }
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>Material Table Example Using JSONPlaceholder API</h1> <br /><br />
+
+      <MaterialTable
+        title="User Details"
+        columns={columns}
+        data={user}
+        options={{
+          headerStyle: { borderBottomColor: 'red', borderBottomWidth: '3px', fontFamily: 'verdana' },
+          actionsColumnIndex: -1
+        }}
+        editable={{
+          onRowUpdate: (newData, oldData) =>
+            new Promise((resolve) => {
+              handleRowUpdate(newData, oldData, resolve);
+
+            }),
+          onRowAdd: (newData) =>
+            new Promise((resolve) => {
+              handleRowAdd(newData, resolve)
+            }),
+          onRowDelete: (oldData) =>
+            new Promise((resolve) => {
+              handleRowDelete(oldData, resolve)
+            }),
+        }}
+      />
+
+      <div>
+        {iserror &&
+          <Alert severity="error">
+            <AlertTitle>ERROR</AlertTitle>
+            {errorMessages.map((msg, i) => {
+              return <div key={i}>{msg}</div>
+            })}
+          </Alert>
+        }
+      </div>
+
     </div>
   );
 }
